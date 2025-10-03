9d5279187d39e900b1f2b75aa34a61e09ed6ea89
elvishammer816 <108972904+elvishammer816@users.noreply.github.com>
2025-10-03 20:52:02 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -7,15 +7,16 @@ API_ID = int(environ.get("API_ID", "29956811"))
 API_HASH = environ.get("API_HASH", "2b89f200b7ec9eeb1fb22ce46e15688c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "604393172"))
+OWNER = int(environ.get("OWNER", "1270406309"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '604393172').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '1270406309').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '604393172').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '1270406309').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
